347e8d5444e50757fa34bbaa5fa0ac00dfbf3ad2
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-20 03:20:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,6 +24,6 @@ end
 
 def find_the_cheese(array)
  cheese_types = ["cheddar", "gouda", "camembert"]
- array.find {|c| if c.include?(cheese_types)}
+ array.find {|c| c.include?(cheese_types)}
  end
 end
