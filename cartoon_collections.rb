f0925ba5e0fc1b6247d56879669f4f23a8c288b6
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-20 02:14:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,9 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |call|
-    puts "#{call.capitalize}!"
+    return "#{call.capitalize}!"
   end
-  
+
 end
 
 def long_planeteer_calls# code an argument here
